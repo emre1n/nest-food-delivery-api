8afab72a236efc5c8e8eccb5afc4509d325bc3d7
emre1n <emre1n@protonmail.com>
2023-07-26 17:04:37 +0300

    add prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { OrderModule } from './order/order.module';
 import { MealModule } from './meal/meal.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, OrderModule, MealModule],
+  imports: [AuthModule, UserModule, OrderModule, MealModule, PrismaModule],
 })
 export class AppModule {}
