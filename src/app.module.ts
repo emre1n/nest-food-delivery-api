264afca242533e5ca517f69f26d790bcd4cd49d2
emre1n <emre1n@protonmail.com>
2023-07-27 16:27:34 +0300

    add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { OrderModule } from './order/order.module';
@@ -6,6 +7,13 @@ import { MealModule } from './meal/meal.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, OrderModule, MealModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UserModule,
+    OrderModule,
+    MealModule,
+    PrismaModule,
+  ],
 })
 export class AppModule {}
